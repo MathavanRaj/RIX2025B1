78f880f1c7eb21eef47d23aed3ea172fd6485360
itsmeaneesh1 <javaglitz@gmail.com>
2025-12-09 12:12:48 +0530

    First Commit

---
+++ src/com/rvz/mypack/Hello.java
@@ -0,0 +1,21 @@
+package com.rvz.mypack;
+
+public class Hello {
+	
+	private String textArea;
+	
+	
+	public String getText() {
+		return textArea;
+	}
+	public void setText(String text) {
+		this.textArea = text;
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
